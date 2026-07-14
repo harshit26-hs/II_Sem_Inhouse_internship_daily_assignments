abab1b1b88488e420f85074fbaa2c70e5e11b85b
harshit26-hs <b250556@skit.ac.in>
2026-07-14 23:26:31 +0530

    Footer.php

---
+++ 25ESKCA090/Day 10 & 11/Footer.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark text-white text-center py-3 mt-5">
+    <p class="mb-0">&copy;
+        2026 Your Website. All Rights Reserved.</p>
+</footer>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
