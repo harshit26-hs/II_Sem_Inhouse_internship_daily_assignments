11c7fec567bbd9f1ba7ed700294fc913f4ccde08
harshit26-hs <b250556@skit.ac.in>
2026-07-14 23:39:24 +0530

    Success.php

---
+++ 25ESKCA090/Day 10 & 11/Success.php
@@ -0,0 +1,17 @@
+<?php
+include ("header.php");
+?>
+
+<div class = "container mt-5 text-center">
+    <div class = "alert alert alert-success">
+        <h3> Registration Successfully!! </h3>
+        <p> Your form has been submitted successfully.. </p>
+</div>
+
+    <a href = "Registration.php" class = "btn btn-primary"> GO BACK </a>
+
+</div>
+
+<?php
+include ("footer.php");
+?>
