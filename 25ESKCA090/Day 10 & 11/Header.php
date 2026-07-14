690f0ccb1d043dc0abcbf69d20710111743161f2
harshit26-hs <b250556@skit.ac.in>
2026-07-14 23:26:00 +0530

    Header.php

---
+++ 25ESKCA090/Day 10 & 11/Header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="width=device-width, initial-scale=1.0">
+    <title> Website </title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <header class="bg-light border-bottom">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center py-3">
+
+            <!-- LOGO -->
+            <img src="https://cdn.vectorstock.com/i/1000v/36/88/login-rectangular-sign-stamp-vector-56023688.jpg" alt="Rectangular distressed stamp reading LOGIN in large block letters; dark gray stamp rotated slightly on a white background, conveying a bold stamped or urgent tone" class="logo" width="80">
+
+            <!-- NAVIGATION -->
+            <nav>
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a href="home.php" class="nav-link text-dark">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="aboutus.php" class="nav-link text-dark">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact.php" class="nav-link text-dark">Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <!-- Logout Button -->
+             <button type="button" class="btn btn-primary" onclick="window.location.href='login.php'">
+                Login
+             </button>
+
+        </div>
+    </header>
