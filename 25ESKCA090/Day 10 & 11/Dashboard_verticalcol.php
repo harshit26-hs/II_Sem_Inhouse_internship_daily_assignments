59f651345476d78f6dfe3a4635a95dcb4c6ea26d
harshit26-hs <b250556@skit.ac.in>
2026-07-14 23:33:06 +0530

    Dashboard_verticalcol.php

---
+++ 25ESKCA090/Day 10 & 11/Dashboard_verticalcol.php
@@ -0,0 +1,11 @@
+<div class = "container-fluid">
+    <div class = "row">
+        <div class = "col-md-3">
+
+        <a href = "update.php"> Update Password </a>
+        <br>
+        <a href = "update_profile.php"> Update Profile </a>
+        <br>
+</div>
+
+<div class = "col-md-9 pt-4>
