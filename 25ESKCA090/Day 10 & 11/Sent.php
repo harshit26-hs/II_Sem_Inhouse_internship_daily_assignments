53155b2607ac6e9cf3bbda929fa866b9ae6ce056
harshit26-hs <b250556@skit.ac.in>
2026-07-14 23:38:23 +0530

    Sent.php

---
+++ 25ESKCA090/Day 10 & 11/Sent.php
@@ -0,0 +1,16 @@
+<?php
+include("header.php");
+?>
+
+<div class = "container mt-5 text-center">
+    <div class = "alert alert-success">
+        <h3> Your message is sent Successfully </h3>
+    </div>
+
+    <a href = "contact.php" class = "btn btn-primary"> Go Back </a>
+</div>
+
+
+<?php
+include("footer.php");
+?>
